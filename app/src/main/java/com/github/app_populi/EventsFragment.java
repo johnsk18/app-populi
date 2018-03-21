594a2fb255f6c89a29e9f099804de7142ff97ce6
Reed <woolfr@rpi.edu>
2018-03-21 15:39:48 -0400

    Update Positions and Events Fragments

--- app/src/main/java/com/github/app_populi/EventsFragment.java
+++ app/src/main/java/com/github/app_populi/EventsFragment.java
@@ -1,8 +1,28 @@
 package com.github.app_populi;
 
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 /**
  * Created by woolf on 3/21/2018.
  */
 
-public class EventsFragment {
+public class EventsFragment extends Fragment {
+    public static EventsFragment newInstance() {
+        EventsFragment fragment = new EventsFragment();
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_events, container, false);
+    }
 }
--- app/src/main/java/com/github/app_populi/PositionsFragment.java
+++ app/src/main/java/com/github/app_populi/PositionsFragment.java
@@ -1,8 +1,28 @@
 package com.github.app_populi;
 
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 /**
  * Created by woolf on 3/21/2018.
  */
 
-public class PositionsFragment {
+public class PositionsFragment extends Fragment {
+    public static PositionsFragment newInstance() {
+        PositionsFragment fragment = new PositionsFragment();
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_positions, container, false);
+    }
 }
