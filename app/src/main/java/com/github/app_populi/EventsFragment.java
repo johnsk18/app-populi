f6e6562cd46522497345c35650b29a9a8a6fa3fa
Reed <woolfr@rpi.edu>
2018-03-26 13:25:19 -0400

    Updated the events page UI

--- app/src/main/java/com/github/app_populi/EventsFragment.java
+++ app/src/main/java/com/github/app_populi/EventsFragment.java
@@ -17,6 +17,8 @@ import android.widget.PopupWindow;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 
@@ -50,6 +52,21 @@ public class EventsFragment extends Fragment{
         eventsList.add(new EventData("Elections","Vote for me",new Date(2018,3,28)));
         eventsList.add(new EventData("Elections","Vote for me",new Date(2018,3,28)));
 
+        eventsList.add(new EventData("Fundraiser","Give some money :)",new Date(2018,3,12)));
+        eventsList.add(new EventData("Meeting","Take some notes",new Date(2018,3,17)));
+        eventsList.add(new EventData("Debate","Debate some stuff",new Date(2018,3,20)));
+        eventsList.add(new EventData("Elections","Vote for me",new Date(2018,3,28)));
+        eventsList.add(new EventData("Elections","Vote for me",new Date(2018,3,28)));
+        eventsList.add(new EventData("Elections","Vote for me",new Date(2018,3,28)));
+        eventsList.add(new EventData("Elections","Vote for me",new Date(2018,3,28)));
+        eventsList.add(new EventData("Fundraiser","Give some money :)",new Date(2018,3,12)));
+        eventsList.add(new EventData("Meeting","Take some notes",new Date(2018,3,17)));
+        eventsList.add(new EventData("Debate","Debate some stuff",new Date(2018,3,20)));
+        eventsList.add(new EventData("Elections","Vote for me",new Date(2018,3,28)));
+        eventsList.add(new EventData("Elections","Vote for me",new Date(2018,3,28)));
+        eventsList.add(new EventData("Elections","Vote for me",new Date(2018,3,28)));
+        eventsList.add(new EventData("Elections","Vote for me",new Date(2018,3,28)));
+
         EventAdapter eventAdapter = new EventAdapter(getContext(),eventsList);
         listView.setAdapter(eventAdapter);
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
@@ -61,15 +78,26 @@ public class EventsFragment extends Fragment{
                 LayoutInflater inflater = (LayoutInflater) getContext().getSystemService(LAYOUT_INFLATER_SERVICE);
                 View customView = inflater.inflate(R.layout.descriptionpopup,null);
 
-                TextView  desc = (TextView) customView.findViewById(R.id.description);
-                desc.setText(description);
+                //Name
+                TextView Name = (TextView) customView.findViewById(R.id.Name);
+                Name.setText(currentEvent.getEventName());
+                //Description
+                TextView Description = (TextView) customView.findViewById(R.id.description);
+                Description.setText("Event Details: "+currentEvent.getEventDescription());
+                //Date
+                DateFormat df = new SimpleDateFormat("MMM dd");
+                TextView Date = (TextView) customView.findViewById(R.id.Date);
+                Date.setText(df.format(currentEvent.getEventDate()));
+
+
+                //close any previously opened popup windows
                 for(PopupWindow x : openedWindows){
                     x.dismiss();
                 }
                 final PopupWindow mPopupWindow = new PopupWindow(
                         customView,
-                        ViewGroup.LayoutParams.WRAP_CONTENT,
-                        ViewGroup.LayoutParams.WRAP_CONTENT
+                        ViewGroup.LayoutParams.MATCH_PARENT,
+                        ViewGroup.LayoutParams.MATCH_PARENT
                 );
                 openedWindows.add(mPopupWindow);
                 if(Build.VERSION.SDK_INT>=21){
